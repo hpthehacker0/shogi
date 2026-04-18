0994fef83349bd2a88bb6170c03a633deb0b8dc2
Hari prasanna M <m.hariprasanna.hp@gmail.com>
2026-04-18 20:35:51 +0530

    Position

--- shogi/src/main/java/com/hariprasanna/shogi/engine/Position.java
+++ shogi/src/main/java/com/hariprasanna/shogi/engine/Position.java
@@ -4,10 +4,7 @@ public record Position(int row ,int column) {
 
 
     public boolean isWithinBounds(){
-        if(row >=0 && row<=8 && column >=0 && column <=8){
-            return true;
-        }
-        return false;
+        return row >= 0 && row <= 8 && column >= 0 && column <= 8;
     }
 
 }
